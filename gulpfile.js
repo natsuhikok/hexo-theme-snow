e115f37d8df674cd43c4da2170a50a09b9e69168
KATO natsuhiko <kato@natsuhiko.jp>
2016-05-23 11:59:05 +0900

    Move sass files from source into root

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ const browsersync = require('browser-sync');
 // SCSS
 /////////////////////////////////////////
 gulp.task('sass', () => {
-  gulp.src('source/scss/all.scss')
+  gulp.src('scss/all.scss')
     .pipe(plumber())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('source/css/'))
     .pipe(browsersync.reload({stream: true}));
 });
 gulp.task('watch', () => {
-  gulp.watch(['source/scss/**'],['sass']);
+  gulp.watch(['scss/**'],['sass']);
 });
 /////////////////////////////////////////
 // Browser-sync
